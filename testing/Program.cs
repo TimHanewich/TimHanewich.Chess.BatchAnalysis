f282211eb589d004d3d9c2f36723d38beb8cc8d8
Tim Hanewich <tahanewich@live.com>
2022-01-10 19:51:47 -0500

    Added testing module

---
+++ testing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TimHanewich.Chess.BatchAnalysis;
+using TimHanewich.Chess;
+using System.IO;
+using TimHanewich.Chess.BatchAnalysis.MoveNodeTree;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Stream s = System.IO.File.OpenRead(@"C:\Users\tahan\Downloads\wbli21.pgn");
+            
+            MoveNodeTreeConstructor mntc = new MoveNodeTreeConstructor();
+            mntc.MassivePgnFileStream = s;
+            mntc.StartConstruction();
+            Task.Delay(10000).Wait();
+            mntc.StopConstructionAsync().Wait();
+
+            Console.WriteLine(JsonConvert.SerializeObject(mntc.ResultMoveNodeTree));
+
+
+        }
+    }
+}
